90c42e82c523ee6d140b8fc80753f3009d641d41
zhanyinjia <15256982729@126.com>
2024-07-10 14:33:30 +0800

    build

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@
  * @Author: zhanyinjia
  * @Date: 2023-08-09 17:59:39
  * @LastEditors: zhanyinjia
- * @LastEditTime: 2024-07-10 11:25:03
+ * @LastEditTime: 2024-07-10 14:30:20
  */
 import { fileURLToPath, URL } from 'node:url'
 
@@ -13,11 +13,14 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import { pathResolve } from "./build/utils";
+const INVALID_CHAR_REGEX = /[\u0000-\u001F"#$&*+,:;<=>?[\]^`{|}\u007F]/g
+const DRIVE_LETTER_REGEX = /^[a-z]:/i
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/vue3-elementplus-repackage/",
-  publicPath: process.env.NODE_ENV=='production'?'/vue3-elementplus-repackage':'/',
+  base: "/vue3-elementplus-repackage/",
+  publicPath: process.env.NODE_ENV == 'production' ? '/vue3-elementplus-repackage' : '/',
   plugins: [
     vue(),
     vueJsx(),
@@ -31,7 +34,7 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },   
+    },
   },
   css: {
     preprocessorOptions: {
@@ -44,5 +47,37 @@ export default defineConfig({
     open: true,
     port: 8080,
     host: true
-  }
+  },
+  build: {
+    // https://cn.vitejs.dev/guide/build.html#browser-compatibility
+    target: "esnext",
+    sourcemap: false,
+    // 消除打包大小超过500kb警告
+    chunkSizeWarningLimit: 4000,
+    rollupOptions: {
+      input: {
+        index: pathResolve("./index.html", import.meta.url)
+      },
+      // 静态资源分类打包
+      output: {
+        chunkFileNames: `static/js/[name]-[hash].js`,
+        entryFileNames: `static/js/[name]-[hash].js`,
+        assetFileNames: `static/[ext]/[name]-[hash].[ext]`,
+        // TODO: 处理GitHub Pages 部署 _plugin-vue_export-helper.js 404
+        // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+        sanitizeFileName(name: any) {
+          const match = DRIVE_LETTER_REGEX.exec(name)
+          const driveLetter = match ? match[0] : ''
+          // A `:` is only allowed as part of a windows drive letter (ex: C:\foo)
+          // Otherwise, avoid them because they can refer to NTFS alternate data streams.
+          return driveLetter + name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, '')
+        },
+        manualChunks(id: any) {
+          if (id.includes('node_modules')) {
+            return id.toString().match(/\/node_modules\/(?!.pnpm)(?<moduleName>[^\/]*)\//)?.groups!.moduleName ?? 'vender'
+          }
+        }
+      }
+    }
+  },
 })
